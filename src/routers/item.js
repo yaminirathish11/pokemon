5f417c395230f15ccfdc126737862a6321b4a4fb
Yamini Rathish <yamini.rathish@gmail.com>
2023-07-27 12:36:11 +0200

    cleared the 404 errors in both nature and characteristic

--- src/routers/item.js
+++ src/routers/item.js
@@ -5,12 +5,21 @@ export async function loader({ params }) {
     const pokemonObject = await axios.get(
       `https://pokeapi.co/api/v2/pokemon/${params.pokemonId}`
     );
-    const response = await axios.get(
-      `https://pokeapi.co/api/v2/characteristic/${params.pokemonId}`
-    );
-    const statsResponse = await axios.get(
-      `https://pokeapi.co/api/v2/nature/${params.pokemonId}`
-    );
+    let response = null;
+    if (params.pokemonId < 31) {
+      response = await axios.get(
+        `https://pokeapi.co/api/v2/characteristic/${params.pokemonId}`
+      );
+    }
+    // const response = await axios.get(
+    //   `https://pokeapi.co/api/v2/characteristic/${params.pokemonId}`
+    // );
+    let statsResponse = null;
+    if (params.pokemonId < 26) {
+      statsResponse = await axios.get(
+        `https://pokeapi.co/api/v2/nature/${params.pokemonId}`
+      );
+    }
     const berryResponse = await axios.get(
       `https://pokeapi.co/api/v2/berry/${params.pokemonId}`
     );
@@ -21,10 +30,10 @@ export async function loader({ params }) {
       pokemonObject.data?.moves.length > 0
         ? pokemonObject.data.moves.map((moveObj) => moveObj.move.name)
         : [];
-        console.log(moves, "moves---------->  ")
+    console.log(moves, "moves---------->  ");
     return {
-      pokemon: des,
-      pokemonStats: statsResponse.data,
+      pokemon: des ? des.description : "No data",
+      pokemonStats: statsResponse ? statsResponse.data : "No data",
       berries: berryResponse.data,
       name: pokemonObject.data.name,
       moves,
